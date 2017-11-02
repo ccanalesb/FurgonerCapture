b5328ad949e2d98f804a9164fcd06f909adee942
Matias <matiasgf18@gmail.com>
2017-11-02 10:11:14 -0300

    Imprime promedio de 10 datos de x,y,z

--- test.py
+++ test.py
@@ -4,14 +4,55 @@ from time import sleep
 import os
 from random import randint
 
+array_prom = []	
+array_y = []
+array_z = []
+
+def promlista_x(array_prom):
+    sum=0
+    for i in range(0,len(array_prom)):
+        sum=sum+array_prom[i]
+ 
+    print sum/len(array_prom)
+
+
+def promlista_y(array_y):
+    sum=0
+    for i in range(0,len(array_y)):
+        sum=sum+array_y[i]
+ 
+    print sum/len(array_y)
+
+
+def promlista_z(array_z):
+   	sum=0
+   	for i in range(0,len(array_z)):
+		sum=sum+array_z[i]
+ 
+   	print sum/len(array_z)
+
 while True:
-    print("Accelerometer data")
-    x = int(randint(0, 9))
-    y = int(randint(0, 9))
-    z = int(randint(0, 9))
-    print("x: " + str(x))
-    print("y: " + str(y))
-    print("z: " + str(z))
-    sleep(0.1)	
-    clear = lambda: os.system('clear')
-    clear()
+	print("Accelerometer data")
+	x = int(randint(0, 90))
+	y = int(randint(0, 90))
+	z = int(randint(0, 90))
+	
+	array_prom.append(x)
+	array_y.append(y)
+	array_z.append(z)
+	
+	print("x: " + str(x))
+	print("y: " + str(y))
+	print("z: " + str(z))
+	
+	if len(array_prom) == 10:
+		promlista_x(array_prom)
+		promlista_y(array_y)
+		promlista_z(array_z)
+		array_prom = []
+		array_y = []	
+		array_z = []
+
+	sleep(0.5)	
+	clear = lambda: os.system('clear')
+	clear()
