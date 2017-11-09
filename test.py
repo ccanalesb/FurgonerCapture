793eaecaba183ac32c49652e60f23fb8d4e1b269
Matias <matiasgf18@gmail.com>
2017-11-09 01:12:58 -0300

    Aparte de leer datos, ahora imprime mensajes de escritura cada x tiempo y alertas

--- test.py
+++ test.py
@@ -1,58 +1,98 @@
 #!/usr/bin/env python
 # from mpu6050 import mpu6050
-from time import sleep
+from time import sleep, clock
 import os
 from random import randint
+import numpy as np #PARA SACAR VARIANZA
+import threading
 
-array_prom = []	
+array_x = []	
 array_y = []
 array_z = []
+prom_x = 0
+prom_y = 0
+prom_z = 0
+a = 0
+test1 = 0
+test2 = 0
+test3 = 0
+alert = 0
+cont = 0
+cont_1 = 0
 
-def promlista_x(array_prom):
-    sum=0
-    for i in range(0,len(array_prom)):
-        sum=sum+array_prom[i]
- 
-    print sum/len(array_prom)
-
-
-def promlista_y(array_y):
-    sum=0
-    for i in range(0,len(array_y)):
-        sum=sum+array_y[i]
- 
-    print sum/len(array_y)
+def setalert(): #Funcion ql para que no webieara la variable global
 
 
-def promlista_z(array_z):
-   	sum=0
-   	for i in range(0,len(array_z)):
-		sum=sum+array_z[i]
- 
-   	print sum/len(array_z)
+	global alert
+	alert = 0 
 
-while True:
+class FirstThread (threading.Thread):
+
+        def run (self):
+                while True:
+                        if alert == 0 or cont == 20: # Si no hay alerta, o si ya se grabo 20 veces en la bdd se escribe en la bdd
+                        	print "TODO BIEN "
+                        	cont = 0
+                        	setalert()
+                        	sleep(2)#Envia datos cada 2 segundos
+                        else:	
+                        	print "ALERTA ALERTA ALERTA " + str(cont)
+                        	cont += 1
+                        	sleep(1)#envia datos cada 1 segundo, por 20 segundos cont = 20
+
+# class SecondThread (threading.Thread):
+#         def run (self):
+#                 while True:
+#                         print "Segundo Hilo: " + str(test2)
+#                         sleep(1)
+
+f = FirstThread()
+f.daemon = True # Daemon se usa para que el proceso muera cuando haga ^C
+f.start()
+
+# s = SecondThread()
+# s.daemon = True
+# s.start()
+
+# def hello_world():
+#   threading.Timer(3.0, hello_world).start() # called every minute
+#   print("HOLA MUNDO")
+# hello_world()
+while True: 
+
 	print("Accelerometer data")
 	x = int(randint(0, 90))
 	y = int(randint(0, 90))
 	z = int(randint(0, 90))
 	
-	array_prom.append(x)
+	array_x.append(x)
 	array_y.append(y)
 	array_z.append(z)
 	
 	print("x: " + str(x))
 	print("y: " + str(y))
 	print("z: " + str(z))
+
 	
-	if len(array_prom) == 10:
-		promlista_x(array_prom)
-		promlista_y(array_y)
-		promlista_z(array_z)
-		array_prom = []
+	if len(array_x) == 10:
+		# prom_x+=1
+		test1 = sum(array_x)/len(array_x)
+		test2 = sum(array_y)/len(array_y)
+		test3 = sum(array_z)/len(array_z)
+		print "Promedio X: " + str(sum(array_x)/len(array_x))
+		print "Promedio Y: " + str(sum(array_y)/len(array_y))	
+		print "Promedio Z: " + str(sum(array_z)/len(array_z))
+		print ("La varianza es: " + str(np.var(array_x)))
+		if np.var(array_x) > 800: #Rango de alerta !
+			print "ALARMAAAAAAAaaaaAAAAAAAAAAAAAAAAAAA" #para que se reinicie el contador de tiempo cada vez que haya una alarma	
+			cont_1 = 0
+			alert = 1
+			
+
+		array_x = []
 		array_y = []	
 		array_z = []
 
-	sleep(0.5)	
-	clear = lambda: os.system('clear')
-	clear()
+	sleep(0.8)	
+	# clear = lambda: os.system('clear')
+	# clear()
