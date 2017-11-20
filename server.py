e2bf2ed9a12eb510fcdaaeb723547c7dea47bbf9
Matias <matiasgf18@gmail.com>
2017-11-20 14:39:16 -0300

    merge

--- server.py
+++ server.py
@@ -3,9 +3,10 @@ import json
 import os
 import configparser
 import psutil
-
+import firebase
 app = Flask(__name__)
-
+child_proc_id = None
+command_route = None
 
 @app.route('/user/<username>')
 def setup_driver(username):
@@ -17,8 +18,25 @@ def setup_driver(username):
         config.write(configfile)
     return 'User added'
 
-@app.route('/init')
-def show_post():
-    
-    # show the post with the given id, the id is an integer
-    return 'init process' 
+@app.route('/init_journey')
+def init_journey():
+    p = psutil.Popen([os.getcwd()+"/furgoner/bin/python", os.getcwd() +
+                      '/my_script.py'], shell=False)
+    command_route = p.cmdline()
+    child_proc_id = p.pid
+    return 'init journey' 
+
+
+@app.route('/terminate_journey')
+def terminate_journey():
+    for process in psutil.process_iter():
+        if process.cmdline() == command_route:
+            process.terminate()
+            break
+    return 'Stopping journey'
+
+
+@app.route('/test')
+def test():
+    print firebase.check_date()
+    return 'Stopping journey'
